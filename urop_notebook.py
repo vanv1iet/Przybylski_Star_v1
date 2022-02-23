28056a28f746b513a4397ae6ab25ffdce62288fc
vanv1iet <waritwij@mit.edu>
2022-02-23 14:17:58 -0500

    clone function
    more development on isline function -- yes/no soon

---
+++ urop_notebook.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 17:15:56 2022
+
+@author: waritwij
+"""
+
+import numpy as np
+import math
+import matplotlib.pylab as plt
+import astropy.io.fits as fits
+import os
+import pandas as pd
+from astropy import units as u
+import astroquery
+from astroquery.atomic import AtomicLineList as alist
+%matplotlib inline
+
